b4a746ac15d969ec66084817003c6c9db615ed67
Ian Davis <iandavisdev@gmail.com>
2021-06-19 15:57:51 -0400

    Strip whitespace of subHeading

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/favicon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Matt Davis',
-  subHeading: 'Founder and CEO of Davis Sales Group  ',
+  subHeading: 'Founder and CEO of Davis Sales Group',
   // social
   socialLinks: [
     {
